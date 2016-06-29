ecdb1a3c9ed2b20747cdac44356b454c665d6942
maccyber <jonas.enge@gmail.com>
2016-06-29 14:15:31 +0200

    Added option to use other templates

--- README.md
+++ README.md
@@ -102,6 +102,7 @@ var data = {
   }
 }
 
+data.templatePath = 'lib/data/skoleskyss.json' // To use another template
 
 console.log(unwrapContact(data))
 
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict'
 
-var repackContact = require('./lib/repack-contact')
+const repackContact = require('./lib/repack-contact')
 
 function unwrapContact (contact) {
   if (!contact) {
     throw new Error('Missing required input: contact object')
   }
+
+  const templatePath = contact.templatePath || ''
+
   if (contact.RESULT && contact.RESULT.HOV) {
-    return repackContact(contact.RESULT.HOV)
+    return repackContact(contact.RESULT.HOV, templatePath)
   } else {
     throw new Error('Malformed contact object')
   }
