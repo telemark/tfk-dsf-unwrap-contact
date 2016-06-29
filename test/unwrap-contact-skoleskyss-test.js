7723edfb7b76aff83f9e0af9eb36448a7778922f
maccyber <jonas.enge@gmail.com>
2016-06-29 14:26:00 +0200

    Added tests for use of template

---
+++ test/unwrap-contact-skoleskyss-test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var tap = require('tap')
+var unwrapContact = require('../index')
+
+tap.throws(
+  function () {
+    unwrapContact()
+  },
+  {message: 'Missing required input: contact object'},
+  'Throws if contact object not supplied'
+)
+
+tap.throws(
+  function () {
+    var malformed = require('./data/malformed.json')
+    unwrapContact(malformed)
+  },
+  {message: 'Malformed contact object'},
+  'Throws if contact object is malformed'
+)
+
+tap.test('It returns expected result', function (test) {
+  var contact = require('./data/dsf.json')
+  var expected = require('./data/unwrapped-skoleskyss.json')
+  contact.templatePath = 'lib/data/skoleskyss.json'
+  var result = unwrapContact(contact)
+  tap.equal(JSON.stringify(result), JSON.stringify(expected), 'Contact unwrapped OK')
+  test.done()
+})
